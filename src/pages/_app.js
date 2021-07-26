41fe2d294e9766bb529f24d77aaf5e29d5a99856
edw19 <edwinpatricionarvaezm@gmail.com>
2021-07-26 17:30:29 -0500

    add ico

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import "nprogress/nprogress.css";
 import Layout from "../components/layout/layout";
 import progress from "nprogress";
 import Router from "next/router";
+import Head from "next/head";
 
 progress.configure({
   showSpinner: true,
@@ -13,9 +14,14 @@ Router.events.on("routeChangeStart", () => progress.start());
 Router.events.on("routeChangeComplete", () => progress.done());
 Router.events.on("routeChangeError", () => progress.done());
 
+const prefix = "/edwin-dev";
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" href={prefix + "/favicon.ico "} />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
